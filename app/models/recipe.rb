1e52225fb88d6422ee19d006c5c1d5958a5b9e9d
Alexandra <arbeuerman@gmail.com>
2021-04-22 10:14:40 -0500

    test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
     has_many :ingredients
     has_many :foods, through: :ingredients
-
+    #hi testing branches
     validates :name, presence: true
     validates :name, uniqueness: true
     validates :meal, inclusion: {in: %w(Breakfast Lunch Dinner Snack), message: "%{value} is not a valid meal type"}
