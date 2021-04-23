aa8b943b2e93c234047bdc99dc564b5048b935c2
Alexandra <arbeuerman@gmail.com>
2021-04-23 08:38:44 -0500

    adding api to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,27 @@ class User < ApplicationRecord
         end
         user_recipes
     end
+
+    private
+    def request_api(url)
+        #   byebug
+        #key: 9973533
+          response = Excon.get(
+            url,
+            headers: {
+              'Host' => URI.parse(url).host
+            }
+          )
+          return nil if response.status != 200
+          parsed = JSON.parse(response.body)
+          new_array = parsed.map { |recipe| recipe }[0] # looks like ["meals", [{recipe 1},{recipe 2}]]  
+          byebug
+          new_array[1]
+        end
+    
+        def find_user_recipes(user_foods)
+          request_api(
+            "https://www.themealdb.com/api/json/v1/1/filter.php?i=#{user_foods}"
+          )
+        end
 end
